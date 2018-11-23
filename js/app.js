2c4d73bda6014f9fb7e7b0ff8838c371898c7d39
gaoyuany <861099134@qq.com>
2018-11-23 17:29:26 +0800

    add

--- js/app.js
+++ js/app.js
@@ -2,5 +2,29 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+	const vm = new Vue({
+		el:'#app',
+		data:{
+			todoName:'',
+			list:[
+				{id:0,name:'gyyyyy',done:false},
+				{id:1,name:'wccccc',done:true},
+				{id:2,name:'grrrrr',done:false}
+			]
+		},
+		methods:{
+			addTodo(e){
+				if(e.keyCode===13){
+					this.list.push({
+						id:this.list.length,
+						name:this.todoName,
+						done:false
+					});
+					
+				}
+			}
+		}
+	})
 
+	window.vm=vm;
 })(window);
